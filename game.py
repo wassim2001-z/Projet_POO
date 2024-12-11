6c790e173090987cf0945ce3868025c84b474b16
ZHAOzihan117 <z406117218@163.com>
2024-12-11 15:35:37 +0100

    add bouton quit game

--- game.py
+++ game.py
@@ -92,6 +92,9 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.KEYDOWN:  
+                    if event.key == pygame.K_ESCAPE: 
+                        running = False  
 
             self.handle_input()
             self.draw()
